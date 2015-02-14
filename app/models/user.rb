51da59868144230bfa8ca0c58d65206f86873e10
Prelang Builder <builder@prelang.com>
2015-02-14 11:36:21 +0000

    Installed relation 'has_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   #->Prelang (user_login/devise)
   belongs_to :grupo
   has_many :actualizacions
+  has_many :posts
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
